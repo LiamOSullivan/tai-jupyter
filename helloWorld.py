e6ac2224948180c6ad8c4b9f97ce8e8631cb9631
LiamOSullivan <lmosulli@tcd.ie>
2021-07-01 15:30:28 +0100

    experimentation

---
+++ helloWorld.py
@@ -0,0 +1,18 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # Testing VS Code Jupyter Integration
+
+# %%
+msg = "hello jupyter"
+print(msg)
+array = [msg]
+print(array[0])
+
+
+# %%
+
+
+# %% [markdown]
+# 
+
